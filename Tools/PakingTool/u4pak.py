3a9e66df9fe68eff3d01ac36fe1c8d6d977fe3bc
SuperCoder79 <k.pranav@gmail.com>
2019-10-18 19:54:35 -0400

    remove non-ascii char

--- Tools/PakingTool/u4pak.py
+++ Tools/PakingTool/u4pak.py
@@ -3,7 +3,7 @@
 #!/usr/bin/env python
 # coding=UTF-8
 #
-# Copyright (c) 2014 Mathias Panzenböck
+# Copyright (c) 2014 Mathias Panzenbock
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
